0fce7074fc6bb1340648b27625e0bb8d3df76cc6
ho <hoseini.py@gmail.com>
2024-03-05 20:49:52 +0330

    inti project

---
+++ shahid_aleali/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from shahid_aleali.users.models import User
+from shahid_aleali.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
